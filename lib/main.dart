289dd471894dd9ef20aab6f1f1df77d994de0b40
Mina Samir <MenaSamerCap@gmail.com>
2019-09-20 17:59:46 +0200

    added dummy screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
+const String APP_NAME = 'Ereny Pacy';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,7 +12,19 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Center(child: Text('Ereny Pacy')),
+      home: DummyScreen(),
+    );
+  }
+}
+
+class DummyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(APP_NAME),
+      ),
+      body: Center(child: Text(APP_NAME)),
     );
   }
 }
